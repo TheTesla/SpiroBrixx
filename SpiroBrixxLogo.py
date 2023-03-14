d9da46d98989a06625ec731d09b440c4d2bb908c
Stefan Helmert <s.helmert@hzdr.de>
2023-03-14 19:34:10 +0100

    trying mask

--- SpiroBrixxLogo.py
+++ SpiroBrixxLogo.py
@@ -8,8 +8,8 @@ ra = 4
 c = 'black'
 w = 2.5
 
-d = draw.Drawing(170, 40, origin=(-75,-21), displayInline=False)
-
+d = draw.Drawing(205, 40, origin=(-89,-21), displayInline=False)
+m = draw.Mask('mask')
 
 
 r = draw.Rectangle(-10, -10, 20, 20, rx=ra, fill='#ffffff', stroke_width=w, stroke=c)
@@ -35,7 +35,7 @@ p.C(-5.5, -10.0+x, -10.0, -5.5+x, -10.0, 0+x*k)
 d.append(p)
 
 for i in [0,1]:
-    x = i * 15 +70
+    x = i * 15 +90
     p = draw.Path(stroke_width=w, stroke=c, fill='black', fill_opacity=0)
     p.M(x-30.0, 0)
     p.C(x, 35.0, x, -35.0, x+30.0, 0)
@@ -44,16 +44,17 @@ for i in [0,1]:
     p.C(x, -35.0, x, 35.0, x+30.0, 0)
     d.append(p)
 
-r = draw.Rectangle(35, -20, 25, 50, fill='#ffffff', stroke_width=w, stroke='#ffffff')
-d.append(r)
-r = draw.Rectangle(95, -20, 25, 50, fill='#ffffff', stroke_width=w, stroke='#ffffff')
+r = draw.Rectangle(55, -20, 25, 50, fill='none', stroke_width=w, stroke='#ffffff')
+m.append(r)
+r = draw.Rectangle(115, -20, 25, 50, fill='#ffffff', stroke_width=w, stroke='#ffffff')
 d.append(r)
 
-p = draw.Text('Spir', 45, -75, 10, fill='black', style="font-family: Isonorm")
+p = draw.Text('Spir', 40, -95, 10, fill='black', style="font-family: osifont")
 d.append(p)
-p = draw.Text('Bri', 45, 16, 10, fill='black', style="font-family: Isonorm")
+p = draw.Text('Bri', 40, 16, 10, fill='black', style="font-family: osifont")
 d.append(p)
 
+d.append(m)
 
 d.set_pixel_scale(2)
 
