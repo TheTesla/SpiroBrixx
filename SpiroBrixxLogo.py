e11609b5986c2fff0a461f4bce6c5f5f18531df5
Stefan Helmert <s.helmert@hzdr.de>
2023-03-16 07:30:58 +0100

    word spacing

--- SpiroBrixxLogo.py
+++ SpiroBrixxLogo.py
@@ -7,8 +7,12 @@ k = 1
 ra = 4
 c = 'black'
 w = 2.5
-ls = -3.5
+ls = -5.5
+ws = 3
 
+
+
+ws += ls -5
 d = draw.Drawing(210, 41, origin=(-95,-21), displayInline=False)
 clip = draw.ClipPath()
 
@@ -17,7 +21,7 @@ d.append(r)
 
 d.append(draw.Circle(0, 0, 10-x, fill='none', stroke_width=w, stroke=c))
 
-r = draw.Rectangle((115+55-7)/2+3*ls, -20, 25+7, 50)
+r = draw.Rectangle((115+55-7)/2+3*ls+ws, -20, 25+7, 50)
 clip.append(r)
 
 p = draw.Path(stroke_width=w, stroke=c, fill=c, fill_opacity=0)
@@ -38,7 +42,7 @@ p.C(-5.5, -10.0+x, -10.0, -5.5+x, -10.0, 0+x*k)
 d.append(p)
 
 for i in [0,1]:
-    x = i * 15 +90+3*ls
+    x = i * 15 +90+3*ls+ws
     p = draw.Path(stroke_width=w, stroke=c, fill=c, fill_opacity=0,
             clip_path=clip)
     p.M(x-30.0, 0)
@@ -52,7 +56,7 @@ for i in [0,1]:
 p = draw.Text('Spir', 40, -101-4*ls, 10, fill=c, style="font-family: osifont",
         letter_spacing=ls)
 d.append(p)
-p = draw.Text('Bri', 40, 18, 10, fill=c, style="font-family: osifont",
+p = draw.Text('Bri', 40, 18+ws, 10, fill=c, style="font-family: osifont",
         letter_spacing=ls)
 d.append(p)
 
