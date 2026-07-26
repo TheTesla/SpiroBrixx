2ddf99a1d864d6b2e0b98fbea248fdbd8f007d77
Stefan Helmert <stefan.helmert@t-online.de>
2026-07-27 00:18:00 +0200

    more elements in make

--- src/mech/spirobrixx/make.py
+++ src/mech/spirobrixx/make.py
@@ -6,14 +6,16 @@ from pathlib import Path
 import argparse
 
 from common.spirostd import make_model
-from models import screwbarL_4, screwbarY_4, screwbarI_4, screwbarI_4X1, \
-        screw_knurl_4, screwdriver, screwbarA_4, \
+from models import \
+        screwbarI_4, screwbarL_4, screwbarY_4, screwbar7_4, screwbarA_4, \
+        screwbarI_4X0, screwbarI_4X0_round, screwbarI_4X1, \
+        screw_knurl_4, screwdriver, \
         screw_headless_4, thin_screwbarO_4_in, thin_screwbarA2_4,\
         screw_knurl_1, screw_flat_1, nut_knurl_1, thin_screwbarA_4,\
         thin_screwbarI_4, thin_screwbarL_4_in, thin_screwbarL_4_out, \
         thin_screwbarLL_4_out, thin_screwbarLL_4_in, thin_screwbarAA_4_out,\
         thin_screwbarAA_4_in, thin_screwbarLA_4_in,\
-        thin_screwbarUU_4_in, thin_screwbarUU_4_out, screwdriver, screw_flat_4,\
+        thin_screwbarUU_4_in, thin_screwbarUU_4_out, screw_flat_4,\
         thin_screwbarU_4_in, thin_screwbarU_4_out,\
         thin_screwgear_4, screw_special_wsc_1, nut_knurl_special_wsc_1,\
         nut_knurl_special_wsc_4
@@ -34,12 +36,39 @@ print(f"Output Path: {profile['target_dir']}")
 
 profile["resolution"] = 0.3
 
+
+# -------- Screwdriver --------------
+parameters = {"l": 150}
+make_model(screwdriver, profile, parameters)
+
+
 # -------- Standard Bars 4 start --------
 for l in range(1,12):
     for w in [1, 2, 3, 5]:
         if w >= l:
             parameters = {"l": l, "w": w, "h": 1}
             make_model(screwbarI_4, profile, parameters)
+
+# -------- Different hole Bars 1, 4 start --------
+for l in [3,4,5]:
+    for w in [1, 2]:
+        for h in [1, 2]:
+            parameters = {"l": l, "w": w, "h": h}
+            make_model(screwbarI_4X0, profile, parameters)
+            make_model(screwbarI_4X0_round, profile, parameters)
+            make_model(screwbarI_4X1, profile, parameters)
+
+# -------- 7 Bars 4 start --------
+for l in [2, 3, 5, 7]:
+    parameters = {"l": l, "w": l, "h": 1}
+    make_model(screwbar7_4, profile, parameters)
+
+# -------- A Bars 4 start --------
+for l in [2, 3, 5]:
+    for w in [2, 3, 5]:
+        if w >= l:
+            parameters = {"l": l, "w": w, "h": 1}
+            make_model(screwbarA_4, profile, parameters)
 
 # -------- L Bars 4 start --------
 for l in [2, 3, 5]:
@@ -91,21 +120,41 @@ for l in [95]:
     make_model(screw_flat_1, profile, parameters)
 
 
+# -------- Thin I Screwbar 4 start --------
+for l in [2, 3, 7]:
+    for w in [3, 5, 9]:
+        if w >= l:
+            parameters = {"l": l, "w": w, "h": 1}
+            make_model(thin_screwbarI_4, profile, parameters)
+
+# -------- Thin A Screwbar 4 start --------
+for l in [2, 3]:
+    for w in [3, 5]:
+        parameters = {"l": l, "w": w, "h": 1}
+        make_model(thin_screwbarA_4, profile, parameters)
+
+# -------- Thin L Screwbar 4 start --------
+for l in [2, 3, 5]:
+    for w in [2, 3, 5]:
+        if w >= l:
+            parameters = {"l": l, "w": w, "h": 1}
+            make_model(thin_screwbarL_4_in, profile, parameters)
+            make_model(thin_screwbarL_4_out, profile, parameters)
+
+# -------- Other Thin Screwbar 4 start --------
+for h in [2, 3, 5]:
+    for l in [2, 3, 5]:
+        for w in [2, 3, 5]:
+            parameters = {"l": l, "w": w, "h": h}
+            make_model(thin_screwbarLL_4_out, profile, parameters)
+            make_model(thin_screwbarLL_4_in, profile, parameters)
+            make_model(thin_screwbarAA_4_out, profile, parameters)
+            make_model(thin_screwbarAA_4_in, profile, parameters)
+            make_model(thin_screwbarLA_4_in, profile, parameters)
+            make_model(thin_screwbarA2_4, profile, parameters)
 
 
-parameters = {"l": 3, "w": 3, "h": 1}
-profile["resolution"] = 0.3
-make_model(screwbarA_4, profile, parameters)
 
-
-
-
-#make_model(thin_screwbarA_4, profile | parameters)
-#make_model(thin_screwbarLL_4_out, profile | parameters)
-#make_model(thin_screwbarAA_4_out, profile | parameters)
-#make_model(thin_screwbarAA_4_in, profile | parameters)
-#make_model(thin_screwbarLA_4_in, profile | parameters)
-#make_model(thin_screwbarLL_4_in, profile | parameters)
 
 
 #parameters = {"l": 1, "w": 5, "h": 1}
@@ -115,59 +164,19 @@ make_model(screwbarA_4, profile, parameters)
 #for z in [7, 9 ,11, 25, 37, 55, 73]:
 #    parameters = {"z": z, "m": 5, "alpha": 20/180*np.pi, "rbofase": 1, "resolution": 0.3}
 #    make_model(thin_screwgear_4, profile, parameters)
-#parameters = {"l": 5, "w": 1, "h": 1}
-#make_model(screwbarI_4, profile, parameters)
-#parameters = {"l": 5, "w": 1, "h": 2}
-#make_model(screwbarI_4, profile, parameters)
-#parameters = {"l": 3, "w": 1, "h": 1}
-#make_model(screwbarI_4, profile, parameters)
-#parameters = {"l": 2, "w": 2, "h": 1}
-#make_model(screwbarL_4, profile, parameters)
 
 #make_model(clamp_screwbarI_4, profile, parameters)
-#make_model(screwbar7_4, profile, parameters)
 
 parameters = {"l": 10, "rtifase": 1.0, "rhofase": 1.0}
-#make_model(nut_knurl_special_wsc_1, profile, parameters)
+make_model(nut_knurl_special_wsc_1, profile, parameters)
 make_model(nut_knurl_special_wsc_4, profile, parameters)
-#
-#parameters = {"l": 150}
-#parameters = {"l": 30, "resolution": 0.3}
-parameters = {"l": 30, "rtofase": 1, "ns1": 0, "as1": 0.2, "rs1": 5.2 } #, "w": 3, "h": 3}
-#make_model(screw_headless_4, profile, parameters)
-#make_model(screw_special_wsc_1, profile, parameters)
-#make_model(screwdriver, profile | parameters)
 
-#parameters = {"l": 2, "w": 1, "h": 1}
-#make_model(screwbarI_4X0, profile | parameters)
-#make_model(screwbarI_4X1, profile | parameters)
-#parameters = {"l": 1, "w": 1, "h": 2}
-#make_model(screwbarI_4X0, profile | parameters)
-#make_model(screwbarI_4X1, profile | parameters)
-#parameters = {"l": 2, "w": 1, "h": 2}
-#make_model(screwbarI_4X0, profile | parameters)
-#make_model(screwbarI_4X1, profile | parameters)
-##make_model(screwbarY_4, profile | parameters)
-##make_model(screwbarL_4, profile | parameters)
-#parameters = {"l": 5, "w": 3, "h": 1}
-#make_model(thin_screwbarA2_4, profile | parameters)
-#parameters = {"l": 6, "w": 3, "h": 1}
-#make_model(thin_screwbarA2_4, profile | parameters)
+parameters = {"l": 30, "rtofase": 1, "ns1": 0, "as1": 0.2, "rs1": 5.2 } #, "w": 3, "h": 3}
+make_model(screw_special_wsc_1, profile, parameters)
+
 #for l in [2,3]:
 #    parameters = {"l": l, "w": l, "h": 1}
-#    make_model(screwbarA_4, profile | parameters)
-#    make_model(thin_screwbarA_4, profile | parameters)
-#    make_model(thin_screwbarLL_4_out, profile | parameters)
-#    make_model(thin_screwbarAA_4_out, profile | parameters)
-#    make_model(thin_screwbarAA_4_in, profile | parameters)
-#    make_model(thin_screwbarLA_4_in, profile | parameters)
-#    make_model(thin_screwbarLL_4_in, profile | parameters)
-    #make_model(screwbarI_4, profile | parameters)
     #make_model(thin_screwbarO_4_in, profile | parameters)
-    #make_model(thin_screwbarA2_4, profile | parameters)
-    #make_model(thin_screwbarL_4_in, profile | parameters)
-    #make_model(thin_screwbarL_4_out, profile | parameters)
-##make_model(thin_screwbarI_4, profile | parameters)
 ##make_model(thin_screwbarUU_4_in, profile | parameters)
 ##make_model(thin_screwbarUU_4_out, profile | parameters)
 #parameters = {"l": 8, "w": 1, "h": 1}
@@ -176,11 +185,6 @@ parameters = {"l": 30, "rtofase": 1, "ns1": 0, "as1": 0.2, "rs1": 5.2 } #, "w": 
 #parameters = {"l": 2, "w": 1, "h": 1}
 #make_model(thin_screwbarU_4_in, profile | parameters)
 #make_model(thin_screwbarU_4_out, profile | parameters)
-##make_model(screwbarI_4, profile | parameters)
-    #create_screwbarI_4_dict(profile, parameters)
-    #create_screwbarI_4(profile, parameters)
-    #create_screwbarL_4(profile, parameters)
-    #create_screwbarY_4(profile, parameters)
 
 
 
